0ba598b0eac4ac5c02718c7c411823e6cc54f0d2
salimsulaiman <salimsulaiman75@gmail.com>
2024-06-13 09:21:28 +0700

    add file

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,61 @@
+import { prisma } from "@/utils/prisma";
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+    const url = new URL(request.url);
+    const roomId = url.searchParams.get("roomId");
+  
+    if (!roomId) {
+      return NextResponse.json({ error: "Missing room id" });
+    }
+  
+    const roomWithChats = await prisma.room.findUnique({
+      where: {
+        id: String(roomId),
+      },
+      include: {
+        chats: true,
+      },
+    });
+  
+    return new Response(JSON.stringify(roomWithChats), {
+      status: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+      },
+    });
+}
+
+export async function POST(request: Request) {
+    const url = new URL(request.url);
+    const roomId = url.searchParams.get("roomId");
+  
+    const res = await request.json();
+  
+    if (!roomId) {
+      return NextResponse.json({ error: "Missing room id" });
+    }
+  
+    if (!res.userId || !res.message) {
+      return NextResponse.json({ error: "Missing userId or message" });
+    }
+  
+    const newChat = await prisma.chat.create({
+      data: {
+        roomId: String(roomId),
+        userId: String(res.userId),
+        message: String(res.message),
+      },
+    });
+  
+    return new Response(JSON.stringify(newChat), {
+      status: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+      },
+    });
+  }
---
+++ src/app/api/room/route.ts
@@ -0,0 +1,62 @@
+import { prisma } from "@/utils/prisma";
+import { Room } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+    const url = new URL(request.url);
+    const userId = url.searchParams.get("userId");
+    const userType = url.searchParams.get("userType");
+  
+    if (!userId) {
+      return NextResponse.json({ error: "Missing userId" });
+    }
+  
+    let userRooms: Room[] = [];
+  
+    if (userType === "DOCTOR") {
+      userRooms = await prisma.room.findMany({
+        where: {
+          doctorId: userId,
+        },
+      });
+    } else {
+      userRooms = await prisma.room.findMany({
+        where: {
+          userId: userId,
+        },
+      });
+    }
+  
+    return new Response(JSON.stringify(userRooms), {
+      status: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+      },
+    });
+}
+
+export async function POST(request: Request) {
+    const res = await request.json();
+  
+    if (!res.userId || !res.doctorId) {
+      return NextResponse.json({ error: "Missing userId or doctorId" });
+    }
+  
+    const newRoom = await prisma.room.create({
+      data: {
+        userId: String(res.userId),
+        doctorId: String(res.doctorId),
+      },
+    });
+  
+    return new Response(JSON.stringify(newRoom), {
+      status: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+      },
+    });
+  }
