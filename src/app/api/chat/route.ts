1a41eabf7ce81f36df426676b12332cc5eb6dd0d
salimsulaiman <salimsulaiman75@gmail.com>
2024-06-13 09:39:54 +0700

    fix bug

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,5 +1,7 @@
-import { prisma } from "@/utils/prisma";
+import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
 
 export async function GET(request: Request) {
     const url = new URL(request.url);
--- src/app/api/room/route.ts
+++ src/app/api/room/route.ts
@@ -1,6 +1,6 @@
-import { prisma } from "@/utils/prisma";
-import { Room } from "@prisma/client";
+import { PrismaClient, Room } from "@prisma/client";
 import { NextResponse } from "next/server";
+const prisma = new PrismaClient();
 
 export async function GET(request: Request) {
     const url = new URL(request.url);
