655d64dc1d0a92b3d1dbb2b1949bb722567db926
salimsulaiman <salimsulaiman75@gmail.com>
2024-06-13 10:40:04 +0700

    fix bug 1.4

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,7 +1,6 @@
 import { prisma } from "@/utils/prisma";
 import { NextResponse } from "next/server";
 
-
 export async function GET(request: Request) {
     const url = new URL(request.url);
     const roomId = url.searchParams.get("roomId");
