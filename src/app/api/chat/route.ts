3a6c7e4812d4846d6f3f803e8cc3cef2dc769bc9
salimsulaiman <salimsulaiman75@gmail.com>
2024-06-13 10:20:11 +0700

    fix bug 1.1

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,8 +1,5 @@
+import { prisma } from "@/utils/prisma";
 import { NextResponse } from "next/server";
-import { PrismaClient } from "@prisma/client";
-
-export const prisma = new PrismaClient()
-
 
 export async function GET(request: Request) {
     const url = new URL(request.url);
