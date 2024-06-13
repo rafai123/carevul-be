20edf75d39f4675440ce7a16d831eefaa7b1b9bb
salimsulaiman <salimsulaiman75@gmail.com>
2024-06-13 11:04:07 +0700

    fix bug 1.8

--- src/app/api/chat/route.js
+++ src/app/api/chat/route.js
@@ -2,7 +2,7 @@ import prisma from "@/utils/prisma";
 import { NextResponse } from "next/server";
 
 // Get chats in room based on room id
-export async function GET(request: Request) {
+export async function GET(request) {
   const url = new URL(request.url);
   const roomId = url.searchParams.get("roomId");
 
@@ -47,7 +47,7 @@ export async function GET(request: Request) {
 }
 
 // Create a chat for the room id in route room/[id]/chat
-export async function POST(request: Request) {
+export async function POST(request) {
   const url = new URL(request.url);
   const roomId = url.searchParams.get("roomId");
 
